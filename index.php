6a3f6456651c05a4190d98187f6a02e92733c3f9
cesar <cesar.aky@gmail.com>
2017-01-11 23:30:24 -0300

    inicio

---
+++ index.php
@@ -0,0 +1,106 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Mi Picada</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="css/bootstrap.css" media="screen">
+    <link rel="stylesheet" href="css/custom.min.css">
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="../" class="navbar-brand">Mi Picada</a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Zonas <span class="caret"></span></a>
+              <ul class="dropdown-menu" aria-labelledby="themes">
+                <li><a href="#">Sur</a></li>
+                <li class="divider"></li>
+                <li><a href="#/">Norte</a></li>
+                <li><a href="#">Centro</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Mi Zona</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#" target="_blank">Contacto</a></li>
+            <li><a href="#" target="_blank">Ingresar</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+
+    <div class="container">
+
+      <div class="page-header" id="banner">
+        <div class="row">
+          <div class="col-lg-8 col-md-7 col-sm-6">
+            <h1>Algun Banner por aca</h1>
+          </div>
+        </div>
+      </div>
+          <div class="col-md-3">
+            <div class="list-group table-of-contents">
+              <a class="list-group-item" href="#navbar">Centro Turistico</a>
+              <a class="list-group-item" href="#buttons">Piscinas</a>
+              <a class="list-group-item" href="#typography">Camping</a>
+              <a class="list-group-item" href="#tables">Hotel</a>
+              <a class="list-group-item" href="#forms">Hostal</a>
+              <a class="list-group-item" href="#navs">Restorant</a>
+              <a class="list-group-item" href="#indicators">Sopaipas</a>
+              <a class="list-group-item" href="#progress-bars">Casa de huifas</a>
+              <a class="list-group-item" href="#containers">Cogollos</a>
+              <a class="list-group-item" href="#dialogs">Etc</a>
+            </div>
+          </div>
+          
+          <div class="col-md-4">
+            <div class="bs-component">
+              <div class="list-group">
+                <a href="#" class="list-group-item active">
+                  Cras justo odio
+                </a>
+                <a href="#" class="list-group-item">Dapibus ac facilisis in
+                </a>
+                <a href="#" class="list-group-item">Morbi leo risus
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-5">
+            <div class="bs-component">
+              <div class="list-group">
+                <a href="#" class="list-group-item">
+                  <h4 class="list-group-item-heading">List group item heading</h4>
+                  <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                </a>
+                <a href="#" class="list-group-item">
+                  <h4 class="list-group-item-heading">List group item heading</h4>
+                  <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+</html>
